0c694d3e38d9a07e86c682b2585b7b00f72a76d3
Nicolò Casagrande <106822126+NicoloCasagrande@users.noreply.github.com>
2024-03-08 11:48:19 +0100

    Update index.js

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const cors = require("cors");
 const axios = require("axios");
+const port = process.env.port || 3000
 
 const app = express();
 app.use(express.json());
@@ -22,4 +23,6 @@ app.post("/authenticate", async (req, res) => {
   }
 });
 
-app.listen(3001);
+app.listen(port, () => {
+  console.log('app is listening');
+});
